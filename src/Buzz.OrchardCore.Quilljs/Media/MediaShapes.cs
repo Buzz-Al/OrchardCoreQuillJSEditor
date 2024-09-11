1fa4666d1f7b0206784ed018039d6d506bf54fa1
Alcwyn <alcwyn@buzzinteractive.co.uk>
2024-09-11 16:55:32 +0100

    fix: Media wrapper for quilljs

---
+++ src/Buzz.OrchardCore.Quilljs/Media/MediaShapes.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using OrchardCore.DisplayManagement.Descriptors;
+using OrchardCore.Modules;
+
+namespace Buzz.OrchardCore.Quilljs.Media
+{
+    [RequireFeatures("OrchardCore.Media")]
+    public class MediaShapes : ShapeTableProvider
+    {
+        public override ValueTask DiscoverAsync(ShapeTableBuilder builder)
+        {
+            builder.Describe("HtmlField_Edit")
+                .OnDisplaying(displaying =>
+                {
+                    var editor = displaying.Shape;
+
+                    if (editor.Metadata.Type == "HtmlField_Edit__Quill")
+                    {
+                        editor.Metadata.Wrappers.Add("Media_Wrapper__HtmlField");
+                    }
+                });
+
+            return ValueTask.CompletedTask;
+        }
+    }
+}
